4f23309736e8112b8dde4f1d2cecec20dd724346
PhanHuuKhoiNguyen <161844694+PhanHuuKhoiNguyen@users.noreply.github.com>
2024-06-23 23:29:15 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -12,13 +12,23 @@ document.getElementById("registerBtn").addEventListener("click", () => {
 
   if (password === "" || confirmPassword === "" || user === "") {
     alert("Không để thông tin trống");
-  } else if (password === confirmPassword) {
-    alert("Đăng ký thành công!");
+  } else if (password !== confirmPassword) {
+    alert("Mật khẩu không trùng khớp");
+  } else if (
+    user.length < 8 ||
+    user.length > 13 ||
+    user[0] !== user[0].toUpperCase()
+  ) {
+    alert("Tên đăng nhập từ 8-13 ký tự và chữ cái đầu viết hoa");
+  } else if (password.length < 8 || password.length > 13) {
+    alert("Mật khẩu từ 8-13 ký tự");
   } else {
-    alert("Mật khẩu không trùng khớp!");
+    if (confirm("Xác nhận đăng ký")) {
+      alert("Đăng ký thành công");
+      window.location.href = "./../html/login-form.html";
+    }
   }
 });
-
 document.getElementById("loginBtn").addEventListener("click", () => {
   container.classList.remove("active");
 });
