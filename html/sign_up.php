c84b95c49eccc23166fe262778976a981885273c
PhanHuuKhoiNguyen <161844694+PhanHuuKhoiNguyen@users.noreply.github.com>
2024-10-18 14:30:34 +0700

    update

--- html/sign_up.php
+++ html/sign_up.php
@@ -24,42 +24,25 @@ session_start();
   unset( $_SESSION["input_email"]);
 
   ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="./../css/signup-mobile.css" media="(max-width: 600px)" />
+    <title>Đăng nhập</title>
+    <link rel="stylesheet" href="./../css/login-form-mobile.css" media="(max-width: 600px)" />
 
-    <link rel="stylesheet" href="./../css/signup.css" media="(min-width: 601px)" />
+    <link rel="stylesheet" href="./../css/loginstd.css" media="(min-width: 601px)" />
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
 </head>
 
 <body>
     <div class="container" id="container">
-        <div class="form-container sign-in">
-            <form id="signUpForm" method="post" action="./../model/index.php">
-                <h1>Đăng nhập</h1>
-                <div class="social-icons">
-                    <a href="" class="icon"><i class="fa-brands fa-tiktok"></i></a>
-                    <a href="" class="icon"><i class="fa-brands fa-instagram"></i></a>
-                    <a href="" class="icon"><i class="fa-brands fa-facebook"></i></a>
-                    <a href="" class="icon"><i class="fa-brands fa-github"></i></a>
-                </div>
-                <input type="hidden" name="action" value="login">
-                <input type="text" name="username" placeholder="Tên tài khoản" value="<?php echo $username ?>" />
-                <label for=""><?php echo $mess_username_login ?></label>
-                <input type="password" name="password" placeholder="Mật khẩu" value="<?php echo $password?>" />
-                <label for=""><?php echo $mess_password_login ?></label>
-                <a href="#!">Quên mật khẩu?</a>
-                <button type="submit" id="loginBtn">Đăng Nhập</button>
-                <a href="./Home_page.php">Quay lại trang chủ</a>
-            </form>
-        </div>
         <div class="form-container sign-up">
-            <form id="signInForm" method="post" action="./../model/index.php">
+            <form id="signUpForm" method="post" action="./../model/index.php" enctype="multipart/form-data">
                 <h1>Tạo tài khoản</h1>
                 <div class="social-icons">
                     <a href="" class="icon"><i class="fa-brands fa-tiktok"></i></a>
@@ -81,22 +64,42 @@ session_start();
                 <a href="./Home_page.php">Quay lại trang chủ</a>
             </form>
         </div>
+        <div class="form-container sign-in">
+            <form id="signInForm" method="post" action="./../model/index.php">
+                <h1>Đăng Nhập</h1>
+                <div class="social-icons">
+                    <a href="" class="icon"><i class="fa-brands fa-tiktok"></i></a>
+                    <a href="" class="icon"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="" class="icon"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="" class="icon"><i class="fa-brands fa-github"></i></a>
+                </div>
+                <input type="hidden" name="action" value="login">
+                <input type="text" name="username" placeholder="Tên tài khoản" value="<?php echo $username ?>" />
+                <label for=""><?php echo $mess_username_login ?></label>
+                <input type="password" name="password" placeholder="Mật khẩu" value="<?php echo $password?>" />
+                <label for=""><?php echo $mess_password_login ?></label>
+                <a href="#!">Quên mật khẩu?</a>
+                <button type="submit" id="loginBtn">Đăng Nhập</button>
+                <a href="./Home_page.php">Quay lại trang chủ</a>
+            </form>
+        </div>
         <div class="toggle-container">
             <div class="toggle">
                 <div class="toggle-panel toggle-left">
-                    <h1>Chào Mừng Trở Lại!</h1>
-                    <p>Nhập thông tin cá nhân để sử dụng tất cả các tính năng</p>
-                    <button class="hidden" id="login">Đăng Nhập</button>
-                </div>
-                <div class="toggle-panel toggle-right">
                     <h1>Xin Chào, Bạn!</h1>
                     <p>Đăng ký với thông tin cá nhân để sử dụng tất cả các tính năng</p>
                     <button class="hidden" id="register">Đăng Ký</button>
                 </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Chào Mừng Trở Lại!</h1>
+                    <p>Nhập thông tin cá nhân để sử dụng tất cả các tính năng</p>
+                    <button class="hidden" id="login">Đăng nhập</button>
+                </div>
             </div>
         </div>
     </div>
-    <script src="./../js/login.js"></script>
+    <script src="./../js/signup.js"></script>
+
 </body>
 
 </html>
